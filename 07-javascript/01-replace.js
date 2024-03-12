6d4f8e7b55d4cba482fc79bbb8ce0bbad081bff0
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-03-12 15:32:14 -0700

    Complete 07-01

--- 07-javascript/01-replace.js
+++ 07-javascript/01-replace.js
@@ -4,12 +4,15 @@ const replaceItems = (input) => {
   // for reference, see the MDN "Regular Expressions" entry:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+  input = input.replaceAll(/gold/gi, "SHINY");
+  input = input.replaceAll(/wander/gi, "roam");
+
   return input;
 };
 
-console.log(replaceItems('All that is gold does not glitter'));
+console.log(replaceItems("All that is gold does not glitter"));
 // expected: 'All that is SHINY does not glitter'
-console.log(replaceItems('Wisdom is better than silver or GOLD.'));
+console.log(replaceItems("Wisdom is better than silver or GOLD."));
 // expected: 'Wisdom is better than silver or SHINY.'
-console.log(replaceItems('Not all those who Wander are lost'));
+console.log(replaceItems("Not all those who Wander are lost"));
 // expected: 'Not all those who roam are lost'
