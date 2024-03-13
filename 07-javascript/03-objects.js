f22c1d4a00216098a5a3c52648c79d464065421f
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-03-12 17:28:18 -0700

    Complete 07-03

--- 07-javascript/03-objects.js
+++ 07-javascript/03-objects.js
@@ -2,21 +2,33 @@ let result = {};
 
 const insertItem = (key, value) => {
   // insertItem the property with key and value into result
+  result[key] = value;
   return key;
 };
 
 const deleteItem = (key) => {
   // remove the property with key from result
+  delete result[key];
   return key;
 };
 
 const lookupItem = (key) => {
   // return the value from result that is associated with key
-  return key;
+  if (result[key] === undefined) {
+    return "Item does not exist"
+  }
+  return result[key];
 };
 
 const printItems = () => {
   // return a string of the concatenated key in result, separated by commas
+  keys = Object.keys(result);
+  res = ""
+  for (let i = 0; i < keys.length - 1; i++) {
+    res += keys[i] + ", ";
+  }
+  res += keys[keys.length - 1];
+  return res
 };
 
 insertItem('hello', 'world');
